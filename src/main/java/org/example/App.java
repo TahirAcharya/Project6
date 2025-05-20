5645c18e66fed820d590b8d54fc0cce2a12ad9f8
tahir <tahir2968@acharya.ac.in>
2025-05-20 20:26:17 +0530

    another change

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello to the world of automation of tasks" );
+        System.out.println( "Hello dear developers to the world of automation of tasks" );
     }
 }
